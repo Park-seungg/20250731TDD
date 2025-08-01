88ef22f94ad1032b4ad575d95748f6d52c1acd27
Park-seungg <parkis1101@gmail.com>
2025-08-01 12:19:29 +0900

    getParamAsInt TDD-red

--- src/test/java/com/ll/RqTest.java
+++ src/test/java/com/ll/RqTest.java
@@ -115,5 +115,13 @@ public class RqTest {
 //        assertEquals("모름", paramValue);
         assertThat(paramValue).isEqualTo("모름");
     }
+    @Test
+    @DisplayName("\"목록?page=2\" : rq.getParamAsInt(\"page\", 1)")
+    void t11() {
+        Rq rq = new Rq("목록?page=2");
 
+        int value = rq.getParamAsInt("page", 1);
+
+        assertThat(value).isEqualTo(2);
+    }
 }
