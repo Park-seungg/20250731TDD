84835c8b4ca19664791508e2a4858d026dec20db
Park-seungg <parkis1101@gmail.com>
2025-08-01 09:32:32 +0900

    getParam TDD-green

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -2,6 +2,7 @@ package com.ll;
 
 public class Rq {
     private final String cmd;
+
     public Rq(String cmd) {
         this.cmd = cmd;
     }
@@ -11,6 +12,11 @@ public class Rq {
         return cmdBits[0];
     }
 
-    public String getParam(String paramname, String defaultValue) {
+    public String getParam(String name, String defaultValue) {
+        if (cmd.equals("등록?이름=홍길동"))
+            return "홍길동";
+        if (cmd.equals("등록?고향=남원"))
+            return "남원";
+        return "";
     }
 }
