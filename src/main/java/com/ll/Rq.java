bb101b40f2d667efcec87c183a795ce03c16ce9f
Park-seungg <parkis1101@gmail.com>
2025-08-01 11:13:23 +0900

    getParam3 TDD-blue(1)

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,5 +1,8 @@
 package com.ll;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     private final String cmd;
 
@@ -15,13 +18,14 @@ public class Rq {
     public String getParam(String name, String defaultValue) {
         String queryString = cmd.split("\\?", 2)[1];
         String[] queryStringBits = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
         for (String paramStr : queryStringBits) {
             String[] paramBits = paramStr.split("=", 2);
             String paramName = paramBits[0];
             String paramValue = paramBits[1];
 
-            if (name.equals(paramName)) return paramValue;
+            params.put(paramName, paramValue);
         }
-        return defaultValue;
+        return params.getOrDefault(name, defaultValue);
     }
 }
