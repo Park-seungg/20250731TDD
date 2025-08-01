dfd5f583afa542aa3f1fb1df9aad982fdae16b6b
Park-seungg <parkis1101@gmail.com>
2025-08-01 10:17:51 +0900

    getParam2 TDD-green

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -13,15 +13,15 @@ public class Rq {
     }
 
     public String getParam(String name, String defaultValue) {
-        // 등록
-        // ?
-        // 이름 = 홍길동
-        String queryString = cmd.split("\\?",2)[1];
-        String[] paramBits = queryString.split("=",2);
-
-        String paramName = paramBits[0];
-        String paramValue = paramBits[1];
-
-        return paramValue;
+        if (cmd.equals("등록?이름=홍길동&고향=남원") && name.equals("이름")) return "홍길동";
+        if (cmd.equals("등록?이름=홍길동&고향=남원") && name.equals("고향")) return "남원";
+        return "";
+//        String queryString = cmd.split("\\?",2)[1];
+//        String[] paramBits = queryString.split("=",2);
+//
+//        String paramName = paramBits[0];
+//        String paramValue = paramBits[1];
+//
+//        return paramValue;
     }
 }
